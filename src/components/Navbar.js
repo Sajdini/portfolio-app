04bd1f25110812cc25667f12ffbc9ff154df1c6e
SamirAjdini <ajdinis1993@gmail.com>
2022-06-11 15:22:51 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+import { Link } from "react-scroll";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+      <div className="container">
+        <div className="navbar-brand" style={{ width: "6rem" }}>
+          <img
+            className="logo"
+            src=""
+            alt="Logo..."
+            style={{ maxWidth: "100%" }}
+          />
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style={{ border: "none" }}
+        >
+          <GiHamburgerMenu style={{ color: "#fff" }} />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <Link to="home" className="nav-link">
+                Home <span className="sr-only"></span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="about" className="nav-link">
+                about me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="skills" className="nav-link">
+                skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="biography" offset={-110} className="nav-link" href="#">
+                Biography
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="portfolio" offset={-110} className="nav-link" href="#">
+                portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="footer" className="nav-link">
+                contacts
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
